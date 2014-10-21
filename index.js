c6940110d317c7e80ace43aea8da2c7785743f91
Square0ne <alecschneider@me.com>
2014-10-21 23:53:19 +0200

    added routes and index.html

--- index.js
+++ index.js
@@ -2,9 +2,22 @@ var express = require('express');
 var http = require('http');
 var socketIo = require('socket.io');
 
+// routes
+var routes = require('./routes/index');
+
 var app = express();
 var server = http.Server(app);
 var io = socketIo(server);
+
+//routes
+app.use('/', routes);
+
+/// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
 
 var port = process.env.PORT || 8000;
 app.listen(port, function() {
