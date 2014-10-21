86a34a797fd5d23755ecd9940961286728cb3cf4
Jonas Pohlmann <jonas.pohlmann@yahoo.de>
2014-10-21 23:58:03 +0200

    Edit comments

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
+//Imports
 var express = require('express');
 var http = require('http');
 var socketIo = require('socket.io');
 
+//Create Server
 var app = express();
 var server = http.Server(app);
-var io = socketIo(server);
+var io = socketIo(server); // Websockets
 
-var port = process.env.PORT || 8000;
+
+var port = process.env.PORT || 8000; // Get Serverport
 app.listen(port, function() {
-	console.log('Server started on port', port);
+	console.log('Server started on port', port); // Start Server and print it 
 });
 
